d2733aebfd376d1459951b55e12f6871727112dc
Razvan Matei <monakyo@gmail.com>
2023-03-26 13:31:49 +0100

    testing nested classes

---
+++ test/integrity/nested/files/Line.ts
@@ -0,0 +1,24 @@
+
+export default class Line {
+    name?: string;
+
+    x: Point;
+
+    y: Point;
+
+    createdAt?: CaptureTimestamp;
+}
+
+class Point {
+    name?: string;
+
+    value: number;
+
+    createdAt?: CaptureTimestamp;
+}
+
+class CaptureTimestamp {
+    value: Date;
+
+    createdBy?: string;
+}
