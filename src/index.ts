75c4d1c8f621783b0dba3063d4e07b2ab0cd9891
Razvan Matei <monakyo@gmail.com>
2023-04-01 20:22:44 +0100

    exposing IAnnotation type

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@
 import Parserr from './lib/Parserr';
 
 import IParserOpts from './lib/types/IParserOpts';
-import { ITypeDescription } from './lib/types/ITypeDescription';
 
 
+export * from './lib/types/ITypeDescription';
 export {
     Parserr,
 
-    IParserOpts,
-    ITypeDescription
+    IParserOpts
 };
